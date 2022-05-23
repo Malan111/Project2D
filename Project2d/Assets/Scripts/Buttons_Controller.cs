f4649d03b499fd740652c5eb6202bdbc1ee9a277
Maxim <45174067+Malan111@users.noreply.github.com>
2022-05-23 22:06:38 +0300

    Profect2D
    Dowloand on GitHub

---
+++ Project2d/Assets/Scripts/Buttons_Controller.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Buttons_Controller : MonoBehaviour
+{
+    [SerializeField] private GameObject panel_Update;
+    [SerializeField] private GameObject panel_Cards;
+    [SerializeField] private GameObject panel_Locked;
+    [SerializeField] private GameObject panel_Shop;
+
+    void Start()
+    {
+        panel_Update.SetActive(false);
+        panel_Cards.SetActive(false);
+        panel_Locked.SetActive(false);
+        panel_Shop.SetActive(false);
+    }
+
+    public void OnPanelBattle()
+    {
+        panel_Update.SetActive(false);
+        panel_Cards.SetActive(false);
+        panel_Locked.SetActive(false);
+        panel_Shop.SetActive(false);
+    }
+
+    public void OnPanelUpdate()
+    {
+        panel_Update.SetActive(true);
+        panel_Cards.SetActive(false);
+        panel_Locked.SetActive(false);
+        panel_Shop.SetActive(false);
+    }
+    public void OnPanelCards()
+    {
+        panel_Update.SetActive(false);
+        panel_Cards.SetActive(true);
+        panel_Locked.SetActive(false);
+        panel_Shop.SetActive(false);
+    }
+    public void OnPanelLocked()
+    {
+        panel_Update.SetActive(false);
+        panel_Cards.SetActive(false);
+        panel_Locked.SetActive(true);
+        panel_Shop.SetActive(false);
+    }
+    public void OnPanelShop()
+    {
+        panel_Update.SetActive(false);
+        panel_Cards.SetActive(false);
+        panel_Locked.SetActive(false);
+        panel_Shop.SetActive(true);
+    }
+
+}
